3877a0bdb5021b45ab43de57247c1ea27b769fba
azeredo-e <eduardo.azeredo99@gmail.com>
2024-03-13 15:46:29 -0300

    Updated tests

--- tests/currency_test.jl
+++ tests/currency_test.jl
@@ -5,12 +5,14 @@ using DataFrames
 
 greet()
 
-@assert BCB.getcurrency_list() isa DataFrame
+@assert getcurrency_list() isa DataFrame
 println("Passed 1 test")
 
-@assert BCB.GetCurrency.CACHE["CURRENCY_LIST"] isa DataFrame
+@assert BCB.CACHE["CURRENCY_LIST"] isa DataFrame
 println("Passed 2 test")
 
-@assert BCB.gettimeseries("AFN", "2023", "2024") isa DataFrame
+@assert gettimeseries("AFN", "2023", "2024") isa DataFrame
 println("Passed 3 test")
+
+
 
