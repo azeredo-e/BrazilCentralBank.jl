b63816407b6ccb049a75a1eaf024a2d520a0e78d
azeredo-e <eduardo.azeredo99@gmail.com>
2024-03-15 09:47:54 -0300

    Updated greet function

--- src/BrazilCentralBank.jl
+++ src/BrazilCentralBank.jl
@@ -12,8 +12,6 @@ include("Currency.jl"); #using .GetCurrency
 export getcurrency_list, gettimeseries
 
 
-export greet
-
-greet() = print("Hello, BCB! It\'s Julia!\n")
+greetBCB() = print("Hello, BCB! It\'s Julia!\n")
 
 end # module BCB
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using DataFrames
 using Dates
 using Test
 
-greet()
+greetBCB()
 
 @testset begin
     @test BrazilCentralBank._get_currency_id("USD") == 61
