2376e52c9bb1ad0f091f73b2886f44fcf492b1ae
azeredo-e <eduardo.azeredo99@gmail.com>
2024-03-25 09:40:56 -0300

    Updated tests for 0.1.1

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,7 @@ using Test
 BrazilCentralBank.greetBCB()
 
 @testset begin
+    # Ver. 0.1.0
     @test BrazilCentralBank._get_currency_id("USD") == 61
     @test getcurrency_list() isa DataFrame
     @test BrazilCentralBank.CACHE["CURRENCY_LIST"] isa DataFrame
@@ -18,4 +19,13 @@ BrazilCentralBank.greetBCB()
     catch err
         @test err isa ArgumentError
     end
+    
+    # Ver. 0.1.1
+    EUR = getCurrency(978)
+    @test EUR.symbol == "EUR"
+    @test EUR isa BrazilCentralBank.Currency
+    @test getCurrency("EUR") isa BrazilCentralBank.Currency
+    @test EUR.gettimeseries("USD", Date(2020, 12, 01), Date(2020, 12, 05)) isa DataFrame
+    @test EUR.gettimeseries(["USD", "CHF"], Date(2020, 12, 01), Date(2020, 12, 05)) isa DataFrame
+    @test EUR.gettimeseries(["USD", "CHF"], Date(2020, 12, 01), Date(2020, 12, 05), groupby="symbol") isa DataFrame
 end
